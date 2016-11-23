6874f57b2deba895805823de6535e848ce59faa8
zmartl <luca.marti@bluewin.ch>
2016-11-23 14:18:23 +0100

    Add model

---
+++ AppQuest_PixelMaler/Model/Pixel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuest_PixelMaler.Model
+{
+    public class Pixel
+    {
+        public int YAttribute { get; set; }
+        public int XAttribute { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ AppQuest_PixelMaler/Model/PixelGrid.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AppQuest_PixelMaler.Model
+{
+    public class PixelGrid
+    {
+        public IList<Pixel> Field { get; set; }
+    }
+}
